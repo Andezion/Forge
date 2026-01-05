311154dea64625dbda7021ff1ec84152ada05931
Andezion <245122@edu.p.lodz.pl>
2026-01-05 22:28:15 +0100

    fixed

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import '../constants/app_colors.dart';
 import '../constants/app_text_styles.dart';
 import '../services/settings_service.dart';
