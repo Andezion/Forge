52842c559174c7815b38eb6e2fcc10a2c7d79d60
Andezion <245122@edu.p.lodz.pl>
2026-01-05 16:15:58 +0100

    added settings

---
+++ lib/models/app_settings.dart
@@ -0,0 +1,90 @@
+enum WeightUnit { kg, lb }
+
+enum DistanceUnit { meters, feet }
+
+enum AppLanguage { english, russian }
+
+class AppSettings {
+  final String? nickname;
+  final String? region;
+  final AppLanguage language;
+  final WeightUnit weightUnit;
+  final DistanceUnit distanceUnit;
+  final bool isProfilePublic;
+  final bool showWorkoutHistory;
+  final bool showPersonalRecords;
+  final bool allowFriendRequests;
+
+  const AppSettings({
+    this.nickname,
+    this.region,
+    this.language = AppLanguage.english,
+    this.weightUnit = WeightUnit.kg,
+    this.distanceUnit = DistanceUnit.meters,
+    this.isProfilePublic = true,
+    this.showWorkoutHistory = true,
+    this.showPersonalRecords = true,
+    this.allowFriendRequests = true,
+  });
+
+  AppSettings copyWith({
+    String? nickname,
+    String? region,
+    AppLanguage? language,
+    WeightUnit? weightUnit,
+    DistanceUnit? distanceUnit,
+    bool? isProfilePublic,
+    bool? showWorkoutHistory,
+    bool? showPersonalRecords,
+    bool? allowFriendRequests,
+  }) {
+    return AppSettings(
+      nickname: nickname ?? this.nickname,
+      region: region ?? this.region,
+      language: language ?? this.language,
+      weightUnit: weightUnit ?? this.weightUnit,
+      distanceUnit: distanceUnit ?? this.distanceUnit,
+      isProfilePublic: isProfilePublic ?? this.isProfilePublic,
+      showWorkoutHistory: showWorkoutHistory ?? this.showWorkoutHistory,
+      showPersonalRecords: showPersonalRecords ?? this.showPersonalRecords,
+      allowFriendRequests: allowFriendRequests ?? this.allowFriendRequests,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'nickname': nickname,
+      'region': region,
+      'language': language.name,
+      'weightUnit': weightUnit.name,
+      'distanceUnit': distanceUnit.name,
+      'isProfilePublic': isProfilePublic,
+      'showWorkoutHistory': showWorkoutHistory,
+      'showPersonalRecords': showPersonalRecords,
+      'allowFriendRequests': allowFriendRequests,
+    };
+  }
+
+  factory AppSettings.fromJson(Map<String, dynamic> json) {
+    return AppSettings(
+      nickname: json['nickname'],
+      region: json['region'],
+      language: AppLanguage.values.firstWhere(
+        (e) => e.name == json['language'],
+        orElse: () => AppLanguage.english,
+      ),
+      weightUnit: WeightUnit.values.firstWhere(
+        (e) => e.name == json['weightUnit'],
+        orElse: () => WeightUnit.kg,
+      ),
+      distanceUnit: DistanceUnit.values.firstWhere(
+        (e) => e.name == json['distanceUnit'],
+        orElse: () => DistanceUnit.meters,
+      ),
+      isProfilePublic: json['isProfilePublic'] ?? true,
+      showWorkoutHistory: json['showWorkoutHistory'] ?? true,
+      showPersonalRecords: json['showPersonalRecords'] ?? true,
+      allowFriendRequests: json['allowFriendRequests'] ?? true,
+    );
+  }
+}
